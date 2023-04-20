c9e43cb4cfef6bddc15ac8ab9a70851a94729949
ANdi g <95663488+DroptheKraken@users.noreply.github.com>
2023-04-20 08:42:44 +0200

    Revert "Updates"
    This reverts commit 532d819ce1e6da4703e2752e88c6a63e6fadafe5.

---
+++ src/Components/PokemonDetails.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import '../PokemonDetails.css';
+
+export const PokemonDetails = () => {
+  const { pokemonName } = useParams();
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      const data = await response.json();
+      setPokemon(data);
+    };
+
+    fetchPokemon();
+  }, [pokemonName]);
+
+  if (!pokemon) {
+    return null;
+  }
+
+  const { name, id, abilities, sprites } = pokemon;
+
+  return (
+    <div className="container">
+      <div className="details">
+        <h1 className="header">{name}</h1>
+        <img className="image" src={sprites.front_default} alt={name} />
+        <div className="info">
+          <label>ID:</label> {id}
+        </div>
+        <div className="abilities">
+          <label>Abilities:</label>
+          <ul>
+            {abilities.map((ability) => (
+              <li key={ability.ability.name}>{ability.ability.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
